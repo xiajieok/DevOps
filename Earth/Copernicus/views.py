542211f9e5496f01d5f5736ec71be989316f1399
medivh <xiajieok@gmail.com>
2016-08-15 17:00:01 +0800

    DevOps

--- Earth/Copernicus/views.py
+++ Earth/Copernicus/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from Copernicus.models import Post
+from Copernicus.forms import PostForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     # posts = Post.objects.all().order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('Copernicus.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
