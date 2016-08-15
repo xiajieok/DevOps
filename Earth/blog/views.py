b82080041f881b9c52256d38890a1241a9f14485
medivh <xiajieok@gmail.com>
2016-08-15 14:00:00 +0800

    DevOps

--- Earth/blog/views.py
+++ Earth/blog/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 from  blog import models
 import json
-#from blog import comment_hander
-#from blog.forms import ArticleForm,handle_uploaded_file
+from blog import comment_hander
+from blog.forms import ArticleForm,handle_uploaded_file
+category_list = models.Category.objects.filter(set_as_top_menu=True).order_by('position_index')
 def index(request):
     category_obj = models.Category.objects.get(position_index=1)
     article_list = models.Article.objects.filter(status='published')
